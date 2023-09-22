18db8ba5f3d5146774943e190d17044bb3ded4c8
AlejandraNandez <124108318+AlejandraNandez@users.noreply.github.com>
2023-09-22 16:44:21 -0600

    usuarios.sql

--- bd.sql
+++ bd.sql
@@ -1,10 +1,8 @@
-CREATE TABLE `alumno` (
-  `cod_estudiante` int(11) NOT NULL,
-  `dni` int(11) NOT NULL,
-  `nombres` varchar(20) NOT NULL,
-  `apellidos` varchar(30) NOT NULL
+CREATE TABLE  (
+  `id int NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `contraseña` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `alumno` (`cod_estudiante`, `dni`, `nombres`, `apellidos`) VALUES
-(101, 75854549, 'Juan Carlos', 'Herrera Sanches'),
-(103, 75854551, 'Jimena', 'Sandobal Gutierre');
+INSERT INTO `alumno` (`nombre`, `contraseña`) VALUES
+('Alejandra', 'hola');
